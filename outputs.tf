d8b1271f171568f86955df31118080dac9bdaadd
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2025-03-26 11:16:44 +0100

    123

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "rds_endpoint" {
-  value = aws_db_instance.this.endpoint
-}
+# output "rds_endpoint" {
+#   value = aws_db_instance.this.endpoint
+# }
 
-output "rds_instance_id" {
-  value = aws_db_instance.this.id
-}
+# output "rds_instance_id" {
+#   value = aws_db_instance.this.id
+# }
