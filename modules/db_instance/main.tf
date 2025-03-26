fe42bbc73e5ed8a4e69b83f763b6451f726128e7
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2025-03-26 14:13:29 +0100

    123

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -131,9 +131,9 @@ resource "aws_db_instance" "this" {
 
 # Log groups will not be created if using an identifier prefix
 resource "aws_cloudwatch_log_group" "this" {
-  for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if var.create && var.create_cloudwatch_log_group && !var.use_identifier_prefix])
+  #for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if var.create && var.create_cloudwatch_log_group && !var.use_identifier_prefix])
 
-  name              = "/aws/rds/instance/${var.identifier}/${each.value}"
+  name              = "/aws/rds/instance/${local.identifier}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
   kms_key_id        = var.cloudwatch_log_group_kms_key_id
   skip_destroy      = var.cloudwatch_log_group_skip_destroy
