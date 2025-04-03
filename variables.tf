3f029358d78cb4cb59c7f0887053fddc478767fb
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2025-04-03 13:29:34 +0200

    123

--- variables.tf
+++ variables.tf
@@ -88,7 +88,11 @@ variable "kms_policy" {
   type        = string
   default     = null
 }
-
+variable "kms_alias_name" {
+  description = "Custom KMS alias name"
+  type        = string
+  default     = null
+}
 variable "license_model" {
   description = "License model information for this DB instance. Optional, but required for some DB engines, i.e. Oracle SE1"
   type        = string
