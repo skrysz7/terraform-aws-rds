759ed7c708b78aea7ee79da1cf9d50f022da0c58
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2025-04-03 16:33:23 +0200

    123

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -19,7 +19,7 @@ resource "aws_db_instance" "this" {
   allocated_storage        = var.allocated_storage
   storage_type             = var.storage_type
   storage_encrypted        = var.storage_encrypted
-  kms_key_id               = var.kms_key_id != null ? var.kms_key_id : module.kms_key.kms_key_id
+  kms_key_id               = var.kms_key_id != null ? var.kms_key_id : module.kms_key.kms_key_arn
   license_model            = local.license_model
 
   db_name                             = var.db_name
--- modules/kms_key/outputs.tf
+++ modules/kms_key/outputs.tf
@@ -1,3 +1,3 @@
-output "kms_key_id" {
-  value = aws_kms_key.this.id
+output "kms_key_arn" {
+  value = aws_kms_key.this.arn
 }
