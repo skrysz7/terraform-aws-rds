ea55404c14a169a9e98f6f6188ff44a4c716e1cb
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2025-04-11 13:38:12 +0200

    123

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -172,7 +172,7 @@ resource "aws_db_instance" "this" {
 resource "aws_cloudwatch_log_group" "this" {
   for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if var.create_cloudwatch_log_group])
 
-  name              = "/aws/rds/instance/${local.identifier}"
+  name              = "/aws/rds/instance/${local.identifier}/${each.key}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
   kms_key_id        = var.cloudwatch_log_group_kms_key_id
   skip_destroy      = var.cloudwatch_log_group_skip_destroy
