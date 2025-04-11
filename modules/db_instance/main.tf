735ef5cc71f959ebb8972e8595e2256f79d68259
Szymon Krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2025-04-11 16:30:11 +0200

    123

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -21,7 +21,7 @@ module "db_option_group" {
   extra_options = var.extra_options
   major_engine_version = var.major_engine_version
   option_group_description = var.option_group_description
-  sg_name = local.sg_name
+  sg_ids = [local.sg_name]
 }
 
 module "db_parameter_group" {
